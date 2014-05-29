56f9bf69891445b6a165fc97c822c4954cf9d9b9
Christian Garbs <mitch@cgarbs.de>
2014-05-29 18:24:30 +0200

    remove branding
    avrdude actually looks for device="USBasp" and vendor="www.fischl.de"
    to identify a usbasp compatible device

--- usbconfig.h
+++ usbconfig.h
@@ -127,7 +127,7 @@ the newest features and options.
 #define USB_CFG_DEVICE_VERSION  0x04, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define	USB_CFG_VENDOR_NAME     'w', 'w', 'w', '.', 'c', 'g', 'a', 'r', 'b', 's', '.', 'd', 'e'
+#define	USB_CFG_VENDOR_NAME     'w', 'w', 'w', '.', 'f', 'i', 's', 'c', 'h', 'l', '.', 'd', 'e'
 #define USB_CFG_VENDOR_NAME_LEN 13
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
