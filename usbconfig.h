ea2992454920b06731ee3d3404e65ac3329b1a47
Christian Garbs <mitch@cgarbs.de>
2014-05-29 18:02:06 +0200

    update README

--- usbconfig.h
+++ usbconfig.h
@@ -137,8 +137,8 @@ the newest features and options.
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define	USB_CFG_DEVICE_NAME		's', 'i', 'g', 'n', 'a', 'l', 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'e', 'r'
-#define	USB_CFG_DEVICE_NAME_LEN	16
+#define	USB_CFG_DEVICE_NAME		'U', 'S', 'B', 'a', 's', 'p'
+#define	USB_CFG_DEVICE_NAME_LEN	 6
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name.
  */
